ad5eea781af9f89dce94b64810a3e764cb14146e
greg-wolff <gregwwolff@gmail.com>
2017-08-05 01:18:22 -0700

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: __dirname,
+    filename: 'main.bundle.js'
+  },
+  module: {
+    rules: [{
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: ["css-loader",
+            "sass-loader"
+          ]
+        })
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader']
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'The Web and Mobile Club',
+      //favicon: __dirname + './images/favicon.ico',
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeScriptTypeAttributes: true,
+        removeAttributeQuotes: true,
+        useShortDoctype: true,
+        minifyCSS: true
+      },
+      template: './src/index.ejs'
+    }),
+    new ExtractTextPlugin("main.css")
+  ]
+}
