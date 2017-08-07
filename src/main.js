99bd3bf98bb7cfb4e13f8ba05f9aa8bb14ef503c
greg-wolff <gregwwolff@gmail.com>
2017-08-06 20:38:08 -0700

    finished landing page, added domain redirect files

--- src/main.js
+++ src/main.js
@@ -3,14 +3,16 @@ require('./main.scss')
 var menu = document.getElementsByClassName('hamburger')[0]
 var close = document.getElementsByClassName('hamburger__close')[0]
 var mobileNav = document.getElementsByClassName('mobile-nav')[0]
+const hideNav = window.matchMedia('(min-width: 900px)')
+document.getElementsByClassName('year')[0].innerHTML = new Date().getFullYear()
 
 menu.addEventListener('click', (e) => {
   e.preventDefault()
   menu.classList.add('hidden')
   mobileNav.classList.remove('hidden')
-  TweenMax.staggerFrom('.mobile-nav__item', .5, {
+  TweenMax.staggerFrom('.mobile-nav__item', 0.5, {
     y: -15,
-    opacity: 0,
+    opacity: 0
   }, 0.06)
 })
 
@@ -19,3 +21,6 @@ close.addEventListener('click', (e) => {
   menu.classList.remove('hidden')
   mobileNav.classList.add('hidden')
 })
+
+hideNav.addListener((mq) =>
+  mq.matches ? mobileNav.classList.add('hidden') : menu.classList.remove('hidden'))
