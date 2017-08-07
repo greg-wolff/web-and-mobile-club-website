ee59e7675a367ddda1ec4a99abe238525253db9b
greg-wolff <gregwwolff@gmail.com>
2017-08-07 09:43:27 -0700

    ... e9b8c05 ....

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var ExtractTextPlugin = require("extract-text-webpack-plugin");
 module.exports = {
   entry: './src/main.js',
   output: {
-    path: __dirname + '/dist/',
+    path: __dirname + '/docs/',
     filename: 'main.bundle.js'
   },
   module: {
